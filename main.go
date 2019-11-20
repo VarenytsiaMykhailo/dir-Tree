517116934ffd19a0f6674ef2d4286e0b720d3d71
VarenytsiaMykhailo <l2fonat@bk.ru>
2019-11-21 01:13:44 +0300

    почистил код

--- main.go
+++ main.go
@@ -15,33 +15,14 @@ func main() {
 	}
 	path := os.Args[1]
 	printFiles := len(os.Args) == 3 && os.Args[2] == "-f"
-	if err := dirTree(out, path, printFiles); err != nil {
+	if err := dirTree(out, path, printFiles, nil); err != nil {
 		panic(err.Error())
 	}
 }
 
-func dirTree(out io.Writer, path string, printFiles bool) (err error) {
-	var sliceForGraffiti []bool
-	if err := dirsScannerAndPrinter(out, path, printFiles, sliceForGraffiti); err != nil {
-		return err
-	}
-	/*if !printFiles {
-		if err := dirsScannerAndPrinter(out, path, sliceForGraffiti); err != nil {
-			return err
-		}
-	} else {
-		if err := dirsAndFilesScannerAndPrinter(out, path, sliceForGraffiti); err != nil {
-			return err
-		}
-	}*/
-	return nil
-}
-
-func dirsScannerAndPrinter(out io.Writer, path string, printFiles bool, sliceForGraffiti []bool) (err error) {
-	//fmt.Println("ЗАШЕЛ В " + path)
+func dirTree(out io.Writer, path string, printFiles bool, sliceForGraffiti []bool) (err error) {
 	dirsAndFiles, err := ioutil.ReadDir(path) //инфа по содержимому в папке
 	if err != nil {
-		fmt.Println("ERROR IN PATH: " + path)
 		return err
 	}
 	var dirs []string //сюда заносим названия папок в директории Path
@@ -53,62 +34,43 @@ func dirsScannerAndPrinter(out io.Writer, path string, printFiles bool, sliceFor
 			files = append(files, file.Name()+" ("+strconv.Itoa(int(file.Size()))+"b)")
 		}
 	}
-	/*fmt.Println("НАХОЖУСЬ В " + path)                //отладка
-	fmt.Printf("ЗДЕСЬ СОДЕРЖАТСЯ ПАПКИ: %v\n", dirs) //отладка*/
 	for i, dirName := range dirs {
-		//fmt.Println("ЗАХОЖУ В " + dirName) //отладка
 		if i == len(dirs)-1 {
 			sliceForGraffiti = append(sliceForGraffiti, true)
 		} else {
 			sliceForGraffiti = append(sliceForGraffiti, false)
 		}
 		if printFiles {
-			/*for i, _ := range sliceForGraffiti {
-				sliceForGraffiti[i] = false
-			}*/
 			if len(files) > 0 {
 				sliceForGraffiti[len(sliceForGraffiti)-1] = false
 			}
 		}
-		if err := dirsPrintGraffiti(out, path, dirName, sliceForGraffiti); err != nil {
+		if err := printGraffiti(out, path, dirName, sliceForGraffiti); err != nil {
 			return err
 		}
-		if err := dirsScannerAndPrinter(out, path+"/"+dirName, printFiles, sliceForGraffiti); err != nil {
+		if err := dirTree(out, path+"/"+dirName, printFiles, sliceForGraffiti); err != nil {
 			return err
 		}
 		sliceForGraffiti = sliceForGraffiti[:len(sliceForGraffiti)-1]
 	}
-
-	/*for i, _ := range sliceForGraffiti {
-
-		sliceForGraffiti[i] = false
-	}*/
+	//обработка печати файлов
 	if printFiles {
 		for i, filesName := range files {
-
 			if i == len(files) - 1 {
 				sliceForGraffiti = append(sliceForGraffiti, true)
 			} else {
 				sliceForGraffiti = append(sliceForGraffiti, false)
 			}
-
-			if err := dirsPrintGraffiti(out, path, filesName, sliceForGraffiti); err != nil {
+			if err := printGraffiti(out, path, filesName, sliceForGraffiti); err != nil {
 				return err
 			}
 			sliceForGraffiti = sliceForGraffiti[:len(sliceForGraffiti)-1]
 		}
 	}
-	//fmt.Println("ВЫХОЖУ ИЗ " + path) //отладка
 	return nil
 }
 
-func dirsPrintGraffiti(out io.Writer, path string, dirName string, sliceForGraffiti []bool) (err error) {
-	/*c := strings.Count(path, "/")
-	var str string
-	for i := 0; i < c; i++ {
-		str += "│\t"
-	}
-	str += "├───"*/
+func printGraffiti(out io.Writer, path string, dirName string, sliceForGraffiti []bool) (err error) {
 	var str string
 	for i := 0; i < len(sliceForGraffiti)-1; i++ {
 		if sliceForGraffiti[i] == false {
@@ -122,11 +84,8 @@ func dirsPrintGraffiti(out io.Writer, path string, dirName string, sliceForGraff
 	} else {
 		str += "└─── "
 	}
-
 	if _, err := fmt.Fprintf(out, "%s\n", str+dirName); err != nil {
 		return err
 	}
 	return nil
 }
-
-
